d9e076ef522071a341acf64b8e276908b4e8953a
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-02-06 11:08:35 -0700

    Solution to problem Maximum Consecutive ones

--- Arrays-and-Strings/MaxConsecutiveOnesIII.cpp
+++ Arrays-and-Strings/MaxConsecutiveOnesIII.cpp
@@ -1,11 +1,41 @@
+/**
+ * @file MaxConsecutiveOnesIII.cpp
+ * @author Taylor Sanchez
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+/**
+ * @brief First Solution - O(n)
+ * 
+ */
 class Solution {
     public:
         int longestOnes(vector<int>& nums, int k) {
-
+            int max_len = 0;
+            int zero_counter = 0;
+            int l = 0;
+            int r = 0;
+            for (r = 0; r < nums.size(); r++) {
+                if(nums[r] == 0) {
+                    zero_counter++;
+                }
+                while (zero_counter > k) {
+                    if (nums[l++] == 0) {
+                        zero_counter--;
+                    }
+                }
+            }
+            max_len = max(max_len, r - l + 1);
+            return max_len;
         }
 };
