9d64e50c0d68cc602bf209ef3325c30361e161d7
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-03-04 18:59:20 -0700

    Added the summary for the running sum

---
+++ Arrays-and-Strings/RunningSum1DArray.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file RunningSum1DArray.cpp
+ * @author Taylor Sanchez
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-14
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @brief 
+ * Given an array of nums. We define a running sum of an array
+ * as runningSum[i] = sum(nums[0]…nums[i]).
+ * 
+ * Return the running sum of nums.
+ * 
+ */
+
+class Solution {
+    public:
+        vector<int> runningSum(vector<int>& nums) {
+           // Define the vector that will be the result
+           // Iterate through the loop of the vector
+           // 
+        }
+};
