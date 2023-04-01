40e4814f4b7b2691d673856d1ce106b0feae1292
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-03-31 23:52:23 -0600

    starter code

--- Trees-and-Graphs/min-depth-of-binary-tree.py
+++ Trees-and-Graphs/min-depth-of-binary-tree.py
@@ -7,3 +7,15 @@ node down to the nearest leaf node.
 Note: A leaf is a node with no children.
 """
 
+class TreeNode(object):
+    def __init__(self, val = 0, left=None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
