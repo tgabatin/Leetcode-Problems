29fed2ea32711091bc8a0877b1a4bba3c4f8e95c
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-07 21:33:35 -0600

    completed the insert into a binary tree

--- Trees-and-Graphs/insert-into-binary-search-tree.py
+++ Trees-and-Graphs/insert-into-binary-search-tree.py
@@ -22,3 +22,12 @@ class Solution(object):
         :type val: int
         :rtype: TreeNode
         """
+        if not root:
+            return TreeNode(val)
+        
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+        
