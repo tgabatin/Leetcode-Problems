bd559a5cc04e5ec13cd40e2a6829da34c6f8d466
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-09 21:32:30 -0600

    added the find if path exists in graph problem - not solved

---
+++ Trees-and-Graphs/find-if-path-exists-in-graph.py
@@ -0,0 +1,24 @@
+"""
+There is a bi-directional graph with n vertices, where each vertex is 
+labeled from 0 to n - 1 (inclusive). The edges in the graph are represented
+as a 2D integer array edges, where each edges[i] = [u_i, v_i] denotes
+a bi-directional edge between vertex u_i and vertex v_i. Every vertex
+pair is connected by at most one edge, and no vertex has an edge to 
+itself.
+
+You want to determine if there is a valid path that exists from vertex
+source to vertex destination. 
+
+Given edges and integers n, source, and destination, return true if there
+is a valid path from source to destination, or false otherwise.
+"""
+
+class Solution(object):
+    def validPath(self, n, edges, source, destination):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type source: int
+        :type destination: int
+        :rtype: bool
+        """
