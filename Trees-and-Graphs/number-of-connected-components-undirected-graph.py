04bbfc0e40a429ce4cdf31674999003ddf0c9a8b
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-11 15:47:43 -0600

    Addition of problem

---
+++ Trees-and-Graphs/number-of-connected-components-undirected-graph.py
@@ -0,0 +1,15 @@
+"""
+You have a graph of n nodes. You are given an integer n and an array
+edges where edges[i] = [a_i, b_i] indicates that there is an edge
+between a_i and b_i in the graph.
+
+Return the number of connected components in the graph
+"""
+
+class Solution(object):
+    def countComponents(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
