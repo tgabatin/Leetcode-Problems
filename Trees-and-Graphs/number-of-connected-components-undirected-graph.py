dfe98b75f7923128a6c871fd807e944dd4c67fa6
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-12 12:18:28 -0600

    completed solution to problem for connected graphs

--- Trees-and-Graphs/number-of-connected-components-undirected-graph.py
+++ Trees-and-Graphs/number-of-connected-components-undirected-graph.py
@@ -6,6 +6,11 @@ between a_i and b_i in the graph.
 Return the number of connected components in the graph
 """
 
+"""
+Time Complexity: O(n+m)
+Space Complexity: O(n+m)
+"""
+
 class Solution(object):
     def countComponents(self, n, edges):
         """
@@ -13,3 +18,23 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
+        adj_list = [[] for _ in range(n)]
+        for edge in edges:
+            adj_list[edge[0]].append(edge[1])
+            adj_list[edge[1]].append(edge[0])
+
+        def dfs(node, visited):
+            visited.add(node)
+            for neighbor in adj_list[node]:
+                if neighbor not in visited:
+                    dfs(neighbor, visited)
+
+        visited = set()
+        count = 0
+
+        for node in range(n):
+            if node not in visited:
+                dfs(node, visited)
+                count += 1
+
+        return count
