7dddf0659383e5b6c46950928108c2dc1507fea0
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-13 16:59:16 -0600

    solution to problem

--- Trees-and-Graphs/max-area-of-island.py
+++ Trees-and-Graphs/max-area-of-island.py
@@ -9,3 +9,21 @@ Return the maximum area of an island in grid. If there is no island,
 return 0.
 """
 
+class Solution(object):
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1 + dfs(i-1, j) + dfs(i+1, j) + dfs(i, j-1) + dfs(i, j+1)
+        
+        max_area = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(i ,j))
+            return max_area
