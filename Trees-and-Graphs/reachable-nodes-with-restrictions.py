94607860438a35fdf7d7b562d6e84871a5af81d2
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-15 16:23:46 -0600

    addition of problem

--- Trees-and-Graphs/reachable-nodes-with-restrictions.py
+++ Trees-and-Graphs/reachable-nodes-with-restrictions.py
@@ -1,4 +1,9 @@
 """
 There is an undidrected tree with n nodes labeled from 0 to n - 1 and
 n - 1 edges.
+
+You are given a 2D integer array edges of length n - 1 where edges[i]
+= [a_i, b_i] indicates that there is an edge between nodes a_i and b_i
+in the tree. You are also given an integer array restricted which 
+represents restricted nodes.
 """
