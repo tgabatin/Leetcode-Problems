8644b96868b4dc5adb7fe1b731ce6f76210a34a1
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-23 20:46:51 -0600

    Added the solution to the remove element problem

--- Easy-Problems/remove-element.py
+++ Easy-Problems/remove-element.py
@@ -12,6 +12,11 @@ as the size of nums.
 - Return k
 """
 
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+"""
+
 class Solution(object):
     def removeElement(self, nums, val):
         """
@@ -19,3 +24,9 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
