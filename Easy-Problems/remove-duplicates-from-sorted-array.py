c267b53c47f5024344384f6704a1d29e15ea1c87
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-25 15:32:43 -0600

    Added another solution to the duplicates problem

--- Easy-Problems/remove-duplicates-from-sorted-array.py
+++ Easy-Problems/remove-duplicates-from-sorted-array.py
@@ -36,6 +36,27 @@ class Solution(object):
 
         return k
     
+    """
+    
+    """
+    class Solution2(object):
+        def removeDuplicates(self, nums):
+            """
+            :type nums: List[int]
+            :rtype: int
+            """
+            if not nums:
+                return 0
+            
+            i = 0
+
+            while i < len(nums) - 1:
+                if nums[i] == nums[i+1]:
+                    del nums[i+1]
+                else:
+                    i += 1
+            
+            return len(nums)
 
 
         
