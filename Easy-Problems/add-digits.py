fda589f2929b7ec9431a90401b603b0842ce0b29
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-26 12:28:39 -0600

    Added the first possible solution for addDigits

--- Easy-Problems/add-digits.py
+++ Easy-Problems/add-digits.py
@@ -3,10 +3,25 @@ Given an integer num, repeatedly add all its digits until the result has only
 one digit, and return it.
 """
 
+
+"""
+Time Complexity: O(logn)
+Space Complexity: O(1)
+"""
 class Solution(object):
     def addDigits(self, num):
         """
         :type num: int
         :rtype: int
         """
+        if num is None:
+            return 0
         
+        while num >= 10:
+            digit_sum = 0
+            for digit in str(num):
+                digit_int = int(digit)
+                digit_sum += digit_int
+            num = digit_sum
+
+        return num
