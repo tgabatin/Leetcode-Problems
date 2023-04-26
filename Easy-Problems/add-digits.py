4155f908f0839316afc36dc8981246aaf201f86e
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-26 11:36:38 -0600

    Added the add digits problem description

---
+++ Easy-Problems/add-digits.py
@@ -0,0 +1,12 @@
+"""
+Given an integer num, repeatedly add all its digits until the result has only
+one digit, and return it.
+"""
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        
