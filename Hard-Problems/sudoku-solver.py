a03a77183e39676ffa71f66e3ac39dcc362e5c83
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-29 18:30:44 -0600

    Added the solution for the sudoku solver

--- Hard-Problems/sudoku-solver.py
+++ Hard-Problems/sudoku-solver.py
@@ -14,11 +14,43 @@ The '.' character indicates empty cells.
 
 class Solution(object):
     def solveSudoku(self, board):
-        """
-        No return type, modification of board is in-place.
-        """
-        # Define helper functions for positional index of values
 
-        # Initialization of all possible values
+        def is_valid(pos, val):
+            row, col = pos // 9, pos % 9
+            box_row = (row // 3) * 3
+            box_col = (col // 3) * 3
+            return val not in board[row] and val not in [board[i][col] for i in range(9)] and \
+                   val not in [board[i][j] for i in range(box_row, box_row + 3) for j in range(box_col, box_col + 3)]
 
-        # Recursion to backtrack through the function
+        possible = [[set(str(i) for i in range(1, 10)) if board[row][col] == '.' else set() for col in range(9)] for row in range(9)]
+        for pos in range(81):
+            row, col = pos // 9, pos % 9
+            if board[row][col] != '.':
+                val = board[row][col]
+                for i in range(9):
+                    possible[row][i].discard(val)
+                    possible[i][col].discard(val)
+                box_row = (row // 3) * 3
+                box_col = (col // 3) * 3
+                for i in range(box_row, box_row + 3):
+                    for j in range(box_col, box_col + 3):
+                        possible[i][j].discard(val)
+
+        def backtrack(pos):
+            if pos == 81:
+                return True
+
+            row, col = pos // 9, pos % 9
+            if board[row][col] != '.':
+                return backtrack(pos + 1)
+
+            for val in possible[row][col]:
+                if is_valid(pos, val):
+                    board[row][col] = val
+                    if backtrack(pos + 1):
+                        return True
+                    board[row][col] = '.'
+
+            return False
+
+        backtrack(0)
