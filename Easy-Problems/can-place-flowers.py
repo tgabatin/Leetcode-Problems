fc505f74f8a92346ca5eb8878961a55f0cb219fa
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-04-29 18:39:55 -0600

    Added the can place flowers problem - no solution

---
+++ Easy-Problems/can-place-flowers.py
@@ -0,0 +1,17 @@
+"""
+You have a long flowerbed in which some of the plots are planted, and some are not.
+However, flowers cannot be planted in adjacent plots. 
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 
+meas not empty, and an integer n, return true if n new flowers can be planted in
+the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+"""
+
+class Solution(object):
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        
