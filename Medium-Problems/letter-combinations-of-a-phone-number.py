8c7b16507acbdc885a165f795f5b271f6243e82d
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-05-24 22:41:06 -0600

    Added the phone letter combinations problem

--- Medium-Problems/letter-combinations-of-a-phone-number.py
+++ Medium-Problems/letter-combinations-of-a-phone-number.py
@@ -8,10 +8,38 @@ A mapping of digits to letters (just like on the telephone buttons) is given bel
 Note that 1 does not map to any letters.
 """
 
+"""
+Time Complexity: O(4^n)
+Space Complexity: O(n)
+"""
 class Solution(object):
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        
+        if not digits:
+            return []
+
+        digit_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+        combinations = []
+        self.dfs(digits, digit_map, 0, '', combinations)
+        return combinations
+
+    def dfs(self, digits, digit_map, index, path, combinations):
+        if len(path) == len(digits):
+            combinations.append(path)
+            return
+
+        for letter in digit_map[digits[index]]:
+            self.dfs(digits, digit_map, index + 1, path + letter, combinations)
