024203b2841c336025d441926fba33d55d1a2a14
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-06-13 15:32:16 -0600

    Added the reverse vowels problem

---
+++ LeetCode-75/Array-String/reverse-vowels-of-a-string.py
@@ -0,0 +1,15 @@
+"""
+Reverse Vowels of a String
+
+Given a string s, reverse only all the vowwels in the string and return it. 
+
+The vowels are 'a', 'e', 'i', 'o' and 'u', and they can appear in both
+lower and upper cases, more than once.
+"""
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
