fd12ac48ae515969379b98ea0ca2b41730773567
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-07-02 22:01:01 -0600

    Added the increasing triplet subsequence solution

--- LeetCode-75/Array-String/increaseing-triplet-subsequence.py
+++ LeetCode-75/Array-String/increaseing-triplet-subsequence.py
@@ -11,4 +11,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        if len(nums) < 3:
+            return False
         
+        min_val = float('inf')
+        mid_val = float('inf')
+
+        for num in nums:
+            if num <= min_val:
+                min_val = num
+            elif num <= mid_val:
+                mid_val = num
+            else:
+                return True
+            
+        return False
