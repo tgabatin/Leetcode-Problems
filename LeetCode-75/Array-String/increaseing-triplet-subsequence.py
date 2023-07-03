f94594bbedbdcd73e20930676df326b5779a4cee
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-07-02 21:30:52 -0600

    Added the increasing triplet subsequence problem

---
+++ LeetCode-75/Array-String/increaseing-triplet-subsequence.py
@@ -0,0 +1,14 @@
+"""
+Increasing Triplet Subsequence
+
+Given an integer array nums, return true if there exists a triple of indices (i,j,k)
+such that i < j < k and nums[i] < nums[k]. If no such indices exists, return false.
+"""
+
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
