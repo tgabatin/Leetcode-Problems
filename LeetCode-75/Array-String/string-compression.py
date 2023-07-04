f6c50240dcb3f0f5874b3a69b49501fe7680677f
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-07-04 14:50:42 -0600

    Added the string compression problem

---
+++ LeetCode-75/Array-String/string-compression.py
@@ -0,0 +1,19 @@
+"""
+String Compression
+
+Given an array of characters chars, compress it using the following algorithm:
+
+Being with an empty string s. For each group of consecutive repeating characters in
+chars:
+
+- If the grou's length is 1, append the character to s
+- Otherwise, append the character followed by the group's length
+
+The compressed string s should not be returned separately, but instead, be stored in
+the input character array chars. Note that the group lengths that are 10 or longer
+will be split into multiple characters in chars. 
+
+Aft4er you are done modifying the input array, return the new length of the array. 
+
+You must write an algorithm that uses only constant extra space.
+"""
