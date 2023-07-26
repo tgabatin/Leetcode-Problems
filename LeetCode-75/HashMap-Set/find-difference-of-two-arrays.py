243d5a4db17494b80e08b3fa680509a58549e15b
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-07-25 22:15:34 -0600

    Added the find difference problem

---
+++ LeetCode-75/HashMap-Set/find-difference-of-two-arrays.py
@@ -0,0 +1,22 @@
+"""
+Find the Difference of Two Arrays
+
+Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2
+where:
+
+* answer[0] is a list of all distinct integers in nums1 which are not present in nums2
+* answer[1] is a list of all distinct integers in nums2 which are not present in nums1
+
+Note that the integers in the lists may be returned in any order.
+"""
+
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+
+        
+
