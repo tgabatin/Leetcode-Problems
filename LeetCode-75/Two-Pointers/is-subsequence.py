82844a6e9662bc3b89b722ff9b090c8f3f5f2f22
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-08-05 23:21:02 -0600

    Added the solution for the subsequence - TOFIX

---
+++ LeetCode-75/Two-Pointers/is-subsequence.py
@@ -0,0 +1,29 @@
+"""
+Is Subsequence
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a new string that is formed from the original string by
+deleting some (can be none) of the characters without disturbing the relative positions
+of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is
+not).
+"""
+# TODO: Fix this solution. 
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+
+        pointer_one = 0
+        pointer_two = 0
+
+        while pointer_one < len(s) and pointer_two < len(t):
+            if s[pointer_one] == t[pointer_two]:
+                pointer_one += 1
+            pointer_two += 1
+
+        return pointer_one == len(s)
+    
