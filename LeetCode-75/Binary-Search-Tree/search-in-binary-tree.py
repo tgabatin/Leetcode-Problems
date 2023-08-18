6b25430ef7befcebdf6f4d8a6e8ee662c28919d5
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-08-17 22:58:47 -0600

    Added the TreeNode question problem

---
+++ LeetCode-75/Binary-Search-Tree/search-in-binary-tree.py
@@ -0,0 +1,22 @@
+"""
+You are given the root of a binary search tree (BST) and an integer val. 
+
+Find the node in the BST that the node's value equals val and return the subtree
+rooted within that node. If such a node does not exist, return null.
+"""
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+
+        
