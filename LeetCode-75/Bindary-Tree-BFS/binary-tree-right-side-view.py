75a35ac0ece0c58b305af5265be5e00aadbaaa54
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-08-26 23:44:32 -0600

    Added the solution for binary tree problem

--- LeetCode-75/Bindary-Tree-BFS/binary-tree-right-side-view.py
+++ LeetCode-75/Bindary-Tree-BFS/binary-tree-right-side-view.py
@@ -17,3 +17,21 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[int]
         """
+        if not root:
+            return []
+        
+        result = []
+        queue = [root]
+
+        while queue:
+            level_size = len(queue)
+            for i in range(level_size):
+                node = queue.pop(0)
+                if i == level_size - 1:
+                    result.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return result
