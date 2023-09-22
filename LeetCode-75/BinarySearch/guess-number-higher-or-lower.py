a37eedbb4024e856d6c7dfec99193aa90956df95
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-09-22 11:05:03 -0600

    Added the solution for guess

--- LeetCode-75/BinarySearch/guess-number-higher-or-lower.py
+++ LeetCode-75/BinarySearch/guess-number-higher-or-lower.py
@@ -30,3 +30,18 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        left, right = 1, n
+
+        while left <= right:
+            mid = left + (right - left) // 2
+            result = guess(mid)
+
+            if result == 0:
+                return mid
+            elif result == -1:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return -1
+            
