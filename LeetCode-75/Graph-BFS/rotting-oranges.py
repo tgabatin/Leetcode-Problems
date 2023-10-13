ee4871360422a2b11b36d8488a67450a3b5aef1e
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-10-13 09:06:40 -0600

    Completed the rotting oranges problem

--- LeetCode-75/Graph-BFS/rotting-oranges.py
+++ LeetCode-75/Graph-BFS/rotting-oranges.py
@@ -14,10 +14,42 @@ Return the minimum number of minutes that must elapse until no cell has
 a fresh orange. If this is impossible, return -1.
 """
 
+from collections import deque
+
 class Solution(object):
     def orangesRotting(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
+        if not grid:
+            return 0
+        
+
+        minutes = 0
+        fresh_oranges = 0
+        queue = deque()
+        directions = [(1,0), (-1, 0), (0, 1), (0, -1)]
+        rows, cols = len(grid), len(grid[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 2:
+                    queue.append((i, j, 0))
+                elif grid[i][j] == 1:
+                    fresh_oranges += 1
+
+        while queue:
+            i, j, minutes = queue.popleft() 
+
+            for dx, dy in directions:
+                x, y = i + dx, j + dy
+                if 0 <= x < rows and 0 <= y < cols and grid[x][y] == 1:
+                    grid[x][y] = 2
+                    fresh_oranges -= 1
+                    queue.append((x, y, minutes + 1))
+
+        if fresh_oranges > 0:
+            return -1
+        return minutes
         
