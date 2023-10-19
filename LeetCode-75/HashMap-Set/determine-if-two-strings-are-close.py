9d998bbcedd0c63691ec259462d838b1a45437d1
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-10-19 10:34:03 -0600

    Added solution: Two Close Strings

--- LeetCode-75/HashMap-Set/determine-if-two-strings-are-close.py
+++ LeetCode-75/HashMap-Set/determine-if-two-strings-are-close.py
@@ -23,3 +23,22 @@ class Solution(object):
         :type word2: str
         :rtype: bool
         """
+        if len(word1) != len(word2):
+            return False
+        
+        count1 = {}
+        count2 = {}
+
+        for char in word1:
+            count1[char] = count1.get(char, 0) + 1
+
+        for char in word2:
+            count2[char] = count2.get(char, 0) + 1
+        
+        if set(count1.keys()) != set(count2.keys()):
+            return False
+        
+        if sorted(count1.values()) == sorted(count2.values()):
+            return True
+        else:
+            return False
