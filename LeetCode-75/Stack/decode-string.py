82a37cbc29383519781c95ab1a8cb9008bc8d68b
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-10-24 10:40:53 -0600

    Added the decode string problem

--- LeetCode-75/Stack/decode-string.py
+++ LeetCode-75/Stack/decode-string.py
@@ -20,3 +20,23 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        stack = []
+        current_num = 0
+        current_str = ""
+
+        for char in s:
+            if char.isdigit():
+                current_num = current_num * 10 + int(char)
+            elif char == '[':
+                stack.append(current_str)
+                stack.append(current_num)
+                current_str = ""
+                current_num = 0
+            elif char == ']':
+                num = stack.pop()
+                prev_str = stack.pop()
+                current_str = prev_str + num * current_str
+            else:
+                current_str += char
+
+        return current_str
