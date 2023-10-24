18d3d6bf5c2c794acf311ae9d051d295ec12b56e
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-10-23 20:12:18 -0600

    Added the first solution for max operations of k-sum pairs

--- LeetCode-75/Two-Pointers/max-number-k-sum-pairs.py
+++ LeetCode-75/Two-Pointers/max-number-k-sum-pairs.py
@@ -16,4 +16,37 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        ptr_1 = 0
+        ptr_2 = 0
+        max_operations = 0
+
+        while ptr_1 < len(nums) - 1 and ptr_2 < len(nums):
+            current_sum = nums[ptr_1] + nums[ptr_2]
+
+            if current_sum == k:
+                max_operations += 1
+                nums.pop(ptr_2)
+                nums.pop(ptr_1)
+                ptr_2 = ptr_1 + 1
+            else:
+                ptr_2 += 1
+                
+            # Check to see if pointer has reached end
+            if ptr_2 >= len(nums):
+                ptr_1 += 1
+                ptr_2 = ptr_1 + 1
+                
+            return max_operations
+
         
+# Thought Process
+        # Iterate through the list with both pointers initialized at start
+        # Keep the first pointer at start and move the second
+        # Add the nums at the iteration
+        # Check if it equals the number k
+        # If it doesn't equal continue
+        # If equals, increate 'max_iterations'
+        # Remove the items that the pointers are pointed to
+        # Start the iteration again
+        # If there are no more items and the pointers have reached the end exit
+        # Return the max_iterations
