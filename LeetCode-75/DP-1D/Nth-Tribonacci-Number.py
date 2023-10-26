9c3dd7d40bfa5f85b663f846a47fa1bb390cf2f3
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-10-26 12:32:28 -0600

    Added tribonacci solution

--- LeetCode-75/DP-1D/Nth-Tribonacci-Number.py
+++ LeetCode-75/DP-1D/Nth-Tribonacci-Number.py
@@ -11,8 +11,21 @@ Given n, return the value of T_n.
 """
 
 class Solution(object):
-    def tribonaci(self, n):
+    def tribonacci(self, n):
         """
         :type n: int
         :rtype: int
         """
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        
+        tribonacci_nums = [0] * (n + 1)
+        tribonacci_nums[1] = 1
+        tribonacci_nums[2] = 1
+
+        for i in range(3, n + 1):
+            tribonacci_nums[i] = tribonacci_nums[i-1] + tribonacci_nums[i-2] + tribonacci_nums[i-3]
+
+        return tribonacci_nums[n]
