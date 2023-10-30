f64ca8e642c3d54d3f109ee6cbc00a99b61265ed
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-10-30 11:09:20 -0600

    Added solution

--- LeetCode-75/HashMap-Set/equal-row-and-column-pairs.py
+++ LeetCode-75/HashMap-Set/equal-row-and-column-pairs.py
@@ -8,9 +8,48 @@ A row and column pair is considered equal if they contain the same elements in t
 order (i.e., an equal array).
 """
 
+"""
+# Incorrect Solution - Review
+class Solution(object):
+    def equalPairs(self, grid):
+        
+        #:type grid: List[int][int]
+        #:rtype: int
+
+        n = len(grid)
+        count = 0
+
+        grid_row = [{} for _ in range(n)]
+        grid_col = [{} for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
+                element = grid[i][j]
+                grid_row[i][element] = grid_row[i].get(element, 0) + 1
+                grid_col[j][element] = grid_col[j].get(element, 0) + 1
+
+        for i in range(n):
+            for j in range(n):
+                if grid_row[i] == grid_col[j]:
+                    count += 1
+
+        return count
+"""
+
+# Time Complexity: O(n^3)
+# Space Complexity: O(1)
 class Solution(object):
     def equalPairs(self, grid):
         """
-        :type grid: List[int][int]
+        :type grid: List[List[int]]
         :rtype: int
         """
+        n = len(grid)
+        count = 0
+
+        for i in range(n):
+            for j in range(n):
+                if grid[i] == [grid[k][j] for k in range(n)]:
+                    count += 1
+
+        return count
