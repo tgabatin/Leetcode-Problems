d4c6be845730be2b2c0105beccf0e851fa1bb081
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-11-01 09:41:49 -0600

    Added fix for max sum

--- LeetCode-75/Bindary-Tree-BFS/maximum-level-sum-of-binary-tree.py
+++ LeetCode-75/Bindary-Tree-BFS/maximum-level-sum-of-binary-tree.py
@@ -10,7 +10,6 @@ x is maximal.
 
 from collections import deque
 
-
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -23,23 +22,20 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        # Check to ensure that there is a TreeNode that we can traverse
         if not root:
             return 0
 
-        # If there are no children, return the max_sum of the value of that level
         max_level = 1
         max_sum = root.val
-        level_sum = 0
-        level = 1
+        current_level = 1
 
-        # If there are children, traverse to the next level and keep track of it in queue
-        # Add the values of the nodes together and store this value in maxSum
         queue = deque([root])
 
         while queue:
+            level_sum = 0
+            level_size = len(queue)  # Number of nodes at the current level
 
-            for _ in range(len(queue)):
+            for _ in range(level_size):
                 node = queue.popleft()
                 level_sum += node.val
 
@@ -48,10 +44,10 @@ class Solution(object):
                 if node.right:
                     queue.append(node.right)
 
-                if level_sum > max_sum:
-                    max_sum = level_sum
-                    max_level = level
+            if level_sum > max_sum:
+                max_sum = level_sum
+                max_level = current_level
 
-                level += 1
-            
-            return max_level
+            current_level += 1
+
+        return max_level
