d75d653a83d5da868b79d25ef06e0703f5bda147
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-11-02 21:47:26 -0600

    Added longest subarray problem

---
+++ LeetCode-75/Sliding-Window/longest-subarray-1s-delete-one-element.py
@@ -0,0 +1,16 @@
+"""
+Longest Subarray of 1's After Deleting One Element
+
+Given a binary array nums, you should delete one element from it.
+
+Return the size of the longest non-empty subarray containing only 1's in the resulting
+array. Return 0 if there is no such subarray.
+"""
+
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
