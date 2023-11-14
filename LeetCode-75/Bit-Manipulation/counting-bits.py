9fe6d39123df1601cd4e65c8e92a8476e39b47ea
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-11-13 18:12:04 -0700

    Added the counting bits solution

--- LeetCode-75/Bit-Manipulation/counting-bits.py
+++ LeetCode-75/Bit-Manipulation/counting-bits.py
@@ -2,3 +2,14 @@
 Given an integer n, return an array ans of length n + 1 such that for each 
 i(0 <= i <= n), ans[i] is the number of 1's in the binary representation of i. 
 """
+
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
+        return ans
