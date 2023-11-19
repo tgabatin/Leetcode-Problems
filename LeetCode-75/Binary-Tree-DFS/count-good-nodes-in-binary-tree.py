3f828faece51074ebc634b44aa89cae3249a26c5
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-11-18 21:07:08 -0700

    Added the solution for counting good nodes in a binary tree.

--- LeetCode-75/Binary-Tree-DFS/count-good-nodes-in-binary-tree.py
+++ LeetCode-75/Binary-Tree-DFS/count-good-nodes-in-binary-tree.py
@@ -7,3 +7,34 @@ root to X there are no nodes with a value greater than X.
 Return the number of good nodes in the binary tree.
 """
 
+from platform import node
+
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def goodNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(root, prev):
+            if not node:
+                return 0
+            elif node.val >= prev:
+                return 1 + dfs(node.left, node.val) + dfs(node..right, node.val)
+            else:
+                return dfs(node.left, prev) + dfs(node.right, prev)
+            
+        return dfs(root, root.val)
+    
+    
+"""
+Explanation:
+
+
+"""
