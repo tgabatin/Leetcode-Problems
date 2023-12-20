d5f068e39a9593641dad49c18d9c8c6b80ea403f
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-12-20 12:18:31 -0700

    Added the search sugguestions problem

--- LeetCode-75/Trie/search-suggestions-system.py
+++ LeetCode-75/Trie/search-suggestions-system.py
@@ -1,5 +1,20 @@
 """
 Search Suggestions System
 
+You are given an array of strings products and a string searchWord. 
 
+Design a system that suggests at most three product names from products after each character
+of searchWord is typed. Suggested products should have common prefix with searchWord. If there
+are more than three products with a common prefix return the three lexicographically minimums
+products.
+
+Returns a list of the suggested products after each character of searchWord is typed.
 """
+
+class Solution(object):
+    def suggestedProducts(self, products, searchWord):
+        """
+        :type products: List[str]
+        :type searchWord: str
+        :rtype: List[List[str]]
+        """
