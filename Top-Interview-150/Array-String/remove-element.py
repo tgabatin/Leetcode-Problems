d313577c0c23f47f352ea52f7183669a954455a2
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2023-12-30 17:12:36 -0700

    Added solution for remove duplicates

--- Top-Interview-150/Array-String/remove-element.py
+++ Top-Interview-150/Array-String/remove-element.py
@@ -21,3 +21,13 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
+        # Remove all occurences of val in nums in place
+        # Return the number of elements in nums which are not equal to val
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+        
