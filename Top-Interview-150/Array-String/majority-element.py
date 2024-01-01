8771eba173d7033850433c8586e7595c33915a77
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2024-01-01 14:17:54 -0700

    Added the solution for majority element

--- Top-Interview-150/Array-String/majority-element.py
+++ Top-Interview-150/Array-String/majority-element.py
@@ -6,6 +6,7 @@ Given an array nums of size n, return the majority element.
 The majority element is the element that appears more than floor(n/2) times. You may
 assume that the majority element always exists in the array.
 """
+import math
 
 class Solution(object):
     def majorityElement(self, nums):
@@ -13,6 +14,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        majority_element = 0
+        majority_element = {}
 
-        
+        for i in range(len(nums)):
+            current_element = nums[i]
+
+            if current_element not in majority_element:
+                majority_element[current_element] = 1
+            else:
+                majority_element[current_element] += 1
+            
+            if majority_element[current_element] > math.floor(len(nums / 2)):
+                break
+
+        return current_element
+
