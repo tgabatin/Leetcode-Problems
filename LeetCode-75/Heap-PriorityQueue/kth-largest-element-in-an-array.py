6fda07c6570af5caedfd602e4c7093124441734d
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2024-01-20 14:11:43 -0700

    Added kth largest element problem

--- LeetCode-75/Heap-PriorityQueue/kth-largest-element-in-an-array.py
+++ LeetCode-75/Heap-PriorityQueue/kth-largest-element-in-an-array.py
@@ -1,5 +1,19 @@
 """
-Kth Largest element in an Array
+Kth Largest Element in an Array
 
+Given an integer array nums and an integer k, return the kth largest element in the
+array. 
 
+Note that it is the kth largest element in the sorted order, not the kth distinct
+element. 
+
+Can you solve it without sorting?
 """
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
