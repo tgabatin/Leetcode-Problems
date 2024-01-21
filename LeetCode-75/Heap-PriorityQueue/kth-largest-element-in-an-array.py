7d816093b9facbd8da9321ddcf2052ef2b642771
Taylor Gabatino <43601896+tgabatin@users.noreply.github.com>
2024-01-20 20:38:07 -0700

    Added the priority queue solution

--- LeetCode-75/Heap-PriorityQueue/kth-largest-element-in-an-array.py
+++ LeetCode-75/Heap-PriorityQueue/kth-largest-element-in-an-array.py
@@ -10,6 +10,8 @@ element.
 Can you solve it without sorting?
 """
 
+import heapq
+
 class Solution(object):
     def findKthLargest(self, nums, k):
         """
@@ -17,3 +19,36 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+
+        max_heap = []
+
+        for num in nums:
+            negated_num = -num
+            max_heap.append(negated_num)
+
+        heapq.heapify(max_heap)
+
+        for _ in range(k - 1):
+            heapq.heappop(max_heap)
+
+        return -max_heap[0]
+    
+
+import queue
+
+class Solution2(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+
+        priority_queue = queue.PriorityQueue()
+
+        for num in nums:
+            priority_queue.put(num)
+            if priority_queue.qsize() > k:
+                priority_queue.get()
+
+        return priority_queue.get()
